d1b1d162955467e7bb607efd193c976a29d2f7e0
Guillaume <lavigne.guillaume@orange.fr>
2020-11-23 13:56:10 +0100

    Handle monitors and padding for ws

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -7,6 +7,10 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config
-polybar -c ~/dotfiles/polybar/config.ini mybar #&
+DISPLAY1="$(xrandr -q|grep 'eDP-1\|VGA-1'|cut -d " " -f1)"
+[[ ! -z "$DISPLAY1" ]] && MONITOR="$DISPLAY1" polybar -c ~/dotfiles/polybar/config.ini mybar &
+
+DISPLAY2="$(xrandr -q|grep 'HDMI-1|DP-4'|cut -d " " -f1)"
+[[ ! -z "$DISPLAY1" ]] && MONITOR="$DISPLAY2" polybar -c ~/dotfiles/polybar/config.ini mybar &
 
 echo "Polybar launched..."
