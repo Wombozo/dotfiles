87204ef633f39cc61448e0b22fe6e7435195edde
Guillaume <lavigne.guillaume@orange.fr>
2020-11-23 14:00:15 +0100

    [Polybar] Dynamic active monitors searching

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,11 +6,8 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch Polybar, using default config location ~/.config/polybar/config
-DISPLAY1="$(xrandr -q|grep 'eDP-1\|VGA-1'|cut -d " " -f1)"
-[[ ! -z "$DISPLAY1" ]] && MONITOR="$DISPLAY1" polybar -c ~/dotfiles/polybar/config.ini mybar &
-
-DISPLAY2="$(xrandr -q|grep 'HDMI-1|DP-4'|cut -d " " -f1)"
-[[ ! -z "$DISPLAY1" ]] && MONITOR="$DISPLAY2" polybar -c ~/dotfiles/polybar/config.ini mybar &
+for m in `xrandr --query | grep " connected"|cut -d " " -f1`; do
+    MONITOR="$m" polybar -c ~/dotfiles/polybar/config.ini mybar &
+done
 
 echo "Polybar launched..."
