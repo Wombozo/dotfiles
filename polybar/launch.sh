6aa6f7ae6d35bf91da01448e5b2f14c1827091d7
Guillaume LAVIGNE <glavigne@edevice.com>
2021-10-08 09:50:02 +0200

    Put specific launch polybar

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,8 +6,14 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+#for m in `xrandr --query | grep " connected"|cut -d " " -f1`; do
+#    MONITOR="$m" polybar -c ~/dotfiles/polybar/config.ini mybar &
+#done
+
+
 for m in `xrandr --query | grep " connected"|cut -d " " -f1`; do
-    MONITOR="$m" polybar -c ~/dotfiles/polybar/config.ini mybar &
+    MONITOR="$m" ~/.config/polybar/launch.sh --cuts &
 done
+
 
 echo "Polybar launched..."
