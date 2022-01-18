003e7b662ae7eeaef779c5b016182cffe7681408
Guillaume LAVIGNE <lavigne.guillaume@orange.fr>
2022-01-18 14:09:55 +0100

    Polyubar launch openbox v2

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -8,9 +8,9 @@ while pgrep -u $UID -x polybar; do sleep 1; done
 
 CONNECTED=$(xrandr --query |grep " connected"|cud -d " " -f1|wc -l)
 if [[ $CONNECTED == 1 ]]; then
-  MONITOR="eDP1" polybar -c ~/dotfiles/polybar/openbox/config.ini main &
+  MONITOR="eDP1" polybar -c ~/dotfiles/polybar/openbox-v2/config.ini main &
 else
-  MONITOR="HDMI1" polybar -c ~/dotfiles/polybar/openbox/config.ini main &
+  MONITOR="HDMI1" polybar -c ~/dotfiles/polybar/openbox-v2/config.ini main &
 fi
 
 #for m in `xrandr --query | grep " connected"|cut -d " " -f1`; do
